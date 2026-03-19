1382db1f3482dcbb63fb56e7413b1006ea60c09a
ROG <seangnangly@gmail.com>
2026-03-19 15:26:13 +0700

    update appsettings connection string

--- obj/Debug/net9.0/SimpleWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/SimpleWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8798f9343873506ad0b00ea3c3e41a92ef755835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17a1a0022c94036095d0e8c3bedf8851cb9bdd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
